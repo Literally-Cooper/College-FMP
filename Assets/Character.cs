91915ab9c93b70f97ae7417ecaae95b4c2aab5a0
Ben Cooper <116600256+Literally-Cooper@users.noreply.github.com>
2024-06-05 12:27:51 +0100

    Update Character.cs

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -12,7 +12,7 @@ public class Character : MonoBehaviour
     private CharacterController characterController;
 
     public float speed = 5f;
-    Dictionary<string, int> inventory = new Dictionary<string, int>(){
+    Dictionary<string, int> inventory = new Dictionary<string, int>(){ //This is the players inventory, using a dictionary
         {"wood", 0},
         {"stone", 0}
     };
@@ -20,32 +20,28 @@ public class Character : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
-
-
+        characterController = GetComponent<CharacterController>(); //gets the character controller component from the object this 
+                                                                   //script is attached to, this is to allow for the script to control the player movement
     }
 
     // Update is called once per frame
     void Update()
     {
         if (characterController.enabled == true){
-            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            characterController.Move(move * Time.deltaTime * speed);
+            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //creates a Vector3, which is used to store 3D positions, taking the players input as variables
+            characterController.Move(move * Time.deltaTime * speed); //Moves the player using the created Vector3
         }
-
-
-
     }
 
     public void addItems(string item, int amount){
-        inventory[item] += amount;
+        inventory[item] += amount; //adds whatever amount to whatever item the function calls for, if the item doesn't exist then it will be automatically added to the dictionary
     }
 
     public int getItem(string item){
         try{
-            return inventory[item];
+            return inventory[item]; //returns the amount of whatever speciffic item is called for
         }catch{
-            return 0;
+            return 0; //if the item doesn't exist in the inventory dictionary, it just returns 0
         }
     }
 }
